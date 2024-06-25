8638d0eee8f49ea4d97ab0dd8922922d946061f1
Evan Goode <mail@evangoo.de>
2024-06-25 16:35:38 -0400

    fjordlauncher: 8.3.2 -> 8.4

--- Casks/fjordlauncher.rb
+++ Casks/fjordlauncher.rb
@@ -1,13 +1,13 @@
 cask "fjordlauncher" do
-  version "8.3.2"
+  version "8.4"
 
   on_mojave :or_older do
-    sha256 "dfba45d718c8bf8b3f7101abbd59849451d1ec2c88f5ff7b5a2bc1c4cfc737d7"
+    sha256 "3a6f5d15eb0806f5c40759012af61ba1a2772be3d869ce632abc3b2413c05ac3"
 
     url "https://github.com/unmojang/FjordLauncher/releases/download/#{version}/FjordLauncher-macOS-Legacy-#{version}.zip"
   end
   on_catalina :or_newer do
-    sha256 "af63642f83e6dbd6ea9cfbfc6ba20d2702fca7b1355e7a6d2f341084e712e4a5"
+    sha256 "5de7691a9f98504d20a806784625a727544c2d22adb428114ef4265e696dfd3b"
 
     url "https://github.com/unmojang/FjordLauncher/releases/download/#{version}/FjordLauncher-macOS-#{version}.zip"
   end
