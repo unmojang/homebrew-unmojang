2add27d0dc4075cb8d6414a3124225dd48ed682c
Evan Goode <mail@evangoo.de>
2025-11-06 20:24:49 -0500

    remove verified url parameter

--- Casks/fjordlauncher.rb
+++ Casks/fjordlauncher.rb
@@ -4,14 +4,12 @@ cask "fjordlauncher" do
   on_catalina :or_older do
     sha256 "a501e6a7e9e59c08e35e4a3201f831bae8bcf677c8229237d49018e0577c35f0"
 
-    url "https://github.com/unmojang/FjordLauncher/releases/download/#{version}/FjordLauncher-macOS-Legacy-#{version}.zip",
-        verified: "github.com/unmojang/FjordLauncher/"
+    url "https://github.com/unmojang/FjordLauncher/releases/download/#{version}/FjordLauncher-macOS-Legacy-#{version}.zip"
   end
   on_big_sur :or_newer do
     sha256 "57285ca2d9f14ed9341f3ec8606da88cbb7e12121fa432ff80de1a913ad04174"
 
-    url "https://github.com/unmojang/FjordLauncher/releases/download/#{version}/FjordLauncher-macOS-#{version}.zip",
-        verified: "github.com/unmojang/FjordLauncher/"
+    url "https://github.com/unmojang/FjordLauncher/releases/download/#{version}/FjordLauncher-macOS-#{version}.zip"
   end
 
   name "Fjord Launcher"
